8120a2493427152b7b5121eed07d11a582de2822
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-06-27 21:19:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pxt-mp3player
+## Serial MP3
+
+Package for an external MP3 Player fom Seeed Studio.
+
+http://wiki.seeedstudio.com/Grove-MP3_v2.0/
+
+## Supported target
+
+* for PXT/calliope
---
+++ mp3player.ts
@@ -0,0 +1,12 @@
+/*
+ *	
+ */
+//% color=#0fbc11 icon="\uf001" block="SerialMP3"
+namespace mp3player {
+
+	//%block shim=MicroBitSerial::send
+    function send(buf: ): void {
+        
+    }
+	
+}
