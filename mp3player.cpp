49ac84d77d0216c30d5480fd396fae83f761a939
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-06-28 20:25:12 +0200

    Update mp3player.cpp

--- mp3player.cpp
+++ mp3player.cpp
@@ -1,7 +1,7 @@
 #include "pxt.h"
 //#include "MicroBitSerial.h"
 
-namespace mp3player {
+class mp3player {
 		
 	MicroBitSerial uBitSerial;
 	
@@ -15,7 +15,6 @@ namespace mp3player {
     //% weight=10
     //% help=serial/send command
     //% blockId=serial_redirect block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte"
-    //% blockExternalInputs=1
 	void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
 		uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
 		uBitSerial.send(cmd, 8);
@@ -29,7 +28,6 @@ namespace mp3player {
     //% weight=10
     //% help=serial/redirect-to
     //% blockId=serial_redirect block="serial|redirect to|TX %tx|RX %rx"
-    //% blockExternalInputs=1
     void redirect(SerialPin tx, SerialPin rx) {
       MicroBitPin* txp = getPin(tx); if (!tx) return;
       MicroBitPin* rxp = getPin(rx); if (!rx) return;
