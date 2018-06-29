2d519bc1e62b6b4d4341e80328d7082a8caf89a9
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-06-29 23:37:16 +0200

    Update mp3player.cpp

--- mp3player.cpp
+++ mp3player.cpp
@@ -1,11 +1,7 @@
 #include "pxt.h"
-//#include "MicroBitSerial.h"
 
 //% weight=5 color=#2699BF icon="\uf120"
 namespace mp3player {
-		
-	MicroBitSerial uBitSerial;
-	
 	
 	/**
     * Sending a command to the MP3 Player
@@ -16,10 +12,9 @@ namespace mp3player {
     //% weight=10
     //% help=serial/send command
     //% blockId=serial_sendCommand block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte"
-	void sendCommand(int command, int hbyte, int lbyte){
+	void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
 		uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
 		uBit.serial.send(cmd, 8);
-		//uBitSerial.send(cmd, 8);
 	}
 
 }
