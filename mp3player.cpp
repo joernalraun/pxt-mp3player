8f479ec298125db508ff3a0bf9a614de2636fbba
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-06-29 22:15:04 +0200

    Add files via upload

--- mp3player.cpp
+++ mp3player.cpp
@@ -1,25 +1,43 @@
 #include "pxt.h"
 //#include "MicroBitSerial.h"
 
+//% weight=5 color=#2699BF icon="\uf120"
 namespace mp3player {
 		
 	MicroBitSerial uBitSerial;
 	
 	
-
-    //%
-    void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
-        uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
-	uBitSerial.send(cmd, 8);
-    }
-	
-    //%
+	/**
+    * Sending a command to the MP3 Player
+    * @param command play a file, eg: 0x03
+    * @param high byte of the 16bit file index, eg: 0x00
+    * @param low byte of the 16bit file index, eg: 0x04
+    */
+    //% weight=10
+    //% help=serial/send command
+    //% blockId=serial_redirect block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte"
+    //% blockExternalInputs=1
+	void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
+		uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
+		uBit.serial.send(cmd, 8);
+		//uBitSerial.send(cmd, 8);
+	}
+		
+	/**
+    * Dynamically configuring the serial instance to use pins other than USBTX and USBRX.
+    * @param tx the new transmission pins, eg: SerialPin.P0
+    * @param rx the new reception pin, eg: SerialPin.P1
+    */
+    //% weight=10
+    //% help=serial/redirect-to
+    //% blockId=serial_redirect block="serial|redirect to|TX %tx|RX %rx"
+    //% blockExternalInputs=1
     void redirect(SerialPin tx, SerialPin rx) {
       MicroBitPin* txp = getPin(tx); if (!tx) return;
       MicroBitPin* rxp = getPin(rx); if (!rx) return;
-
-      uBit.serial.redirect(txp->name, rxp->name);
-      uBit.serial.baud(9600);
+	  
+      uBitSerial.redirect(txp->name, rxp->name);
+      uBitSerial.baud(9600);
     }
 
 }
