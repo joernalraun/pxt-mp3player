caa7f4a59d235669b907eba6955e09820e3a95fe
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 12:06:39 +0200

    Update mp3player.ts

--- mp3player.ts
+++ mp3player.ts
@@ -1,7 +1,5 @@
-/*
- *	
- */
-//% color=#0fbc11 icon="\uf120"
+
+
 namespace mp3player {
 
     /**
@@ -9,12 +7,11 @@ namespace mp3player {
      * @param high byte of the 16bit file index, eg: 0x00
      * @param low byte of the 16bit file index, eg: 0x04
      */
-    //% weight=87 blockGap=8
     //% block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte" 
     //% blockId=send_command
-    //% icon="\uf120"
     //% shim=mp3player::sendCommand
     export function sendCommand(command: number, hbyte: number, lbyte: number): void {
         //mp3player.sendCommand(command, hbyte, lbyte)
+        return;
     }
 }
