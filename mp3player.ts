88335d1ac52851422adf62580e71fa9b81337ea7
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 12:42:04 +0200

    Update mp3player.ts

--- mp3player.ts
+++ mp3player.ts
@@ -7,7 +7,7 @@ namespace mp3player {
      * @param high byte of the 16bit file index, eg: 0x00
      * @param low byte of the 16bit file index, eg: 0x04
      */
-    //% block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte" 
+    //% block="serial|write buffer %buffer" 
     //% blockId=send_command
     //% shim=mp3player::sendCommand
     export function sendCommand(command: Buffer): void {
