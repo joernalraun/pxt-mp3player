97327a865bab910fc4c2f209f4e9e004a4fc1edb
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 14:42:14 +0200

    Update mp3player.ts

--- mp3player.ts
+++ mp3player.ts
@@ -3,20 +3,6 @@
 namespace mp3player {
 
     /**
-     * Dynamically configuring the serial instance to use pins other than USBTX and USBRX.
-     * @param tx the new transmission pins, eg: SerialPin.P0
-     * @param rx the new reception pin, eg: SerialPin.P1
-     * @param rate the new baud rate. eg: 115200
-     */
-    //% weight=10
-    //% help=serial/redirect-to
-    //% blockId=serial_redirect block="serial|redirect to|TX %tx|RX %rx|at baud rate %rate"
-    //% blockExternalInputs=1 shim=serial::redirect
-    function redirect(tx: SerialPin, rx: SerialPin, rate: BaudRate): void{
-        return;
-    }
-    
-    /**
      * @param command buffer to send
      */
     //% block="serial|send|command %command" 
