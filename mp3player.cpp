d510f811b5ee3716364e78fce1bf1fd64c77bed4
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 12:36:24 +0200

    Update mp3player.cpp

--- mp3player.cpp
+++ mp3player.cpp
@@ -3,10 +3,14 @@
 namespace mp3player {
 	
 
-    //%
-	void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
+    //% block="serial|write buffer %buffer"
+	void sendCommand(Buffer buffer){
 		//static const uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
 		//uBit.serial.send(cmd, 8);
+		if (!buffer) return;
+		
+		ManagedBuffer buf(buffer);
+      		uBit.serial.send(buf.getBytes(), buf.length());
 	}
 
 }
