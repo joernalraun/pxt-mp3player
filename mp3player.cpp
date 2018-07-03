05b2f6cf200981e7cb3b11ecdf4c0bfb603c1b2b
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 11:45:35 +0200

    Update mp3player.cpp

--- mp3player.cpp
+++ mp3player.cpp
@@ -3,15 +3,8 @@
 //% weight=5 color=#2699BF icon="\uf120" block="mp3player"
 namespace mp3player {
 	
-	/**
-    * Sending a command to the MP3 Player
-    * @param command play a file, eg: 0x03
-    * @param high byte of the 16bit file index, eg: 0x00
-    * @param low byte of the 16bit file index, eg: 0x04
-    */
-    //% weight=10
-    //% help=serial/send command
-    //% blockId=serial_sendCommand block="serial|send|command %command | hbyte %hbyte | lbyte %lbyte"
+
+    //%
 	void sendCommand(uint8_t command, uint8_t hbyte, uint8_t lbyte){
 		static const uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
 		uBit.serial.send(cmd, 8);
