c2f99830a49767ec2c27643fe0c9b80e1f2a94f4
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-07-03 14:42:33 +0200

    Update mp3player.cpp

--- mp3player.cpp
+++ mp3player.cpp
@@ -2,24 +2,6 @@
 
 namespace mp3player {
 	
-    /**
-    * Dynamically configuring the serial instance to use pins other than USBTX and USBRX.
-    * @param tx the new transmission pins, eg: SerialPin.P0
-    * @param rx the new reception pin, eg: SerialPin.P1
-    * @param rate the new baud rate. eg: 115200
-    */
-    //% weight=10
-    //% help=serial/redirect-to
-    //% blockId=serial_redirect block="serial|redirect to|TX %tx|RX %rx|at baud rate %rate"
-    //% blockExternalInputs=1
-    void redirect(SerialPin tx, SerialPin rx, BaudRate rate) {
-      MicroBitPin* txp = getPin(tx); if (!tx) return;
-      MicroBitPin* rxp = getPin(rx); if (!rx) return;
-
-      uBit.serial.redirect(txp->name, rxp->name);
-      uBit.serial.baud((int)rate);
-    }
-	
     //%
 	void sendCommand(Buffer buffer){
 		//static const uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
