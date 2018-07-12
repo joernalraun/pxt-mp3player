60893a9cd5fc582de4b0b65feac830120d15efad
Jørn Alraun <ja@urbn-pockets.com>
2018-07-12 13:18:33 +0200

    Update and rename mp3player.cpp to MP3_Modul.cpp

--- MP3_Modul.cpp
+++ MP3_Modul.cpp
@@ -1,11 +1,9 @@
 #include "pxt.h"
 
-namespace MP3-Modul {
+namespace MP3_Modul {
 	
     //%
 	void sendCommand(Buffer buffer){
-		//static const uint8_t cmd[] = {0x7E, 0xFF, 0x06, command, 0x00, hbyte, lbyte, 0xEF };
-		//uBit.serial.send(cmd, 8);
 		if (!buffer) return;
 		
 		ManagedBuffer buf(buffer);
