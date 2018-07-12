e5e8c6a014d6eb1a7f159db90981c98301576509
Jørn Alraun <ja@urbn-pockets.com>
2018-07-12 13:19:54 +0200

    Update and rename mp3player.ts to MP3_Modul.ts

--- MP3_Modul.ts
+++ MP3_Modul.ts
@@ -1,6 +1,6 @@
 
-//% color=#ff4600 icon="\uf001" block="MP3-Modul"
-namespace MP3-Modul {
+//% color=#ff4600 icon="\uf001" block="MP3_Modul"
+namespace MP3_Modul {
 
 	serial.onDataReceived("", () => {
 	})
@@ -10,7 +10,7 @@ namespace MP3-Modul {
      */
     //% block="serial|send|command %command" 
     //% blockId=send_command
-    //% shim=MP3-Modul::sendCommand
+    //% shim=MP3_Modul::sendCommand
     function send(command: Buffer): void {
         return;
     }
